a298bb143fc65626a8f4de8ad6be91c46a1d9d04
Deeni Akhbar <deeniakhbar@gmail.com>
2025-08-31 09:33:58 +0000

    everything is perfect but every page need to be fully responsive as i ne

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="https://app.burhaniguards.org/images/logo.png" type="image/png" sizes="any" />
         {/* Removed OneSignal SDK script */}
       </head>
