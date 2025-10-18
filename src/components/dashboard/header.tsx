4f5ce546936cda019c0938472deed2945aea27dd
New Quality <newqualitykuwait@gmail.com>
2025-10-18 14:15:35 +0000

    perfect now add toggle switch with notification bell in header

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -275,6 +275,27 @@ export function Header() {
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="rounded-full">
+                <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => handleSetTheme('light')}>
+                Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => handleSetTheme('dark')}>
+                Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => handleSetTheme('system')}>
+                System
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
           <Button variant="ghost" size="icon" className="rounded-full">
             <UserCircle className="h-6 w-6" />
             <span className="sr-only">Toggle user menu</span>
@@ -284,16 +305,6 @@ export function Header() {
           <DropdownMenuLabel>My Account</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={() => router.push('/dashboard/profile')}>Profile</DropdownMenuItem>
-           <DropdownMenuSub>
-            <DropdownMenuSubTrigger>
-              Theme
-            </DropdownMenuSubTrigger>
-            <DropdownMenuSubContent>
-              <DropdownMenuItem onClick={() => handleSetTheme('light')}>Light</DropdownMenuItem>
-              <DropdownMenuItem onClick={() => handleSetTheme('dark')}>Dark</DropdownMenuItem>
-              <DropdownMenuItem onClick={() => handleSetTheme('system')}>System</DropdownMenuItem>
-            </DropdownMenuSubContent>
-          </DropdownMenuSub>
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={handleLogout}>
             <LogOut className="mr-2 h-4 w-4" />
